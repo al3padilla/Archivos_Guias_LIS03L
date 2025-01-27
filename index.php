142352ba28afe26faca625ea923312a53edb2c13
NR221019 <143046534+al3padilla@users.noreply.github.com>
2025-01-26 23:10:18 -0600

    Archivo Ejercicios Guia
    Archivos index y ejercicios guia 1

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Índice - Proyecto de Guías lis 03l</title>
+    <link rel="stylesheet" href="estilo_index.css">
+</head>
+<body>
+    <header>
+        <h1>Proyecto de Guías Alexandra Padilla</h1>
+        <nav>
+            <ul>
+                
+                <li><a href="#ejemplos-guias">Ejemplos de las Guías</a></li>
+                <li><a href="#ejercicios-discusion">Ejercicios de Discusión</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        
+
+        <section id="ejemplos-guias">
+            <h2>Ejemplos de las Guías</h2>
+            <ul>
+            <li><a href="Guia1_NR221019/Ejercicio1/ejercicio1.php">Ejercicio 1 - Guía 1</a></li>
+                <li><a href="guias/guia2/ejemplos/ejemplo2.php">Ejemplo 2 - Guía 2</a></li>
+                <!-- Agrega más enlaces según sea necesario -->
+            </ul>
+        </section>
+
+        <section id="ejercicios-discusion">
+            <h2>Ejercicios de Discusión</h2>
+            <ul>
+                <li><a href="guias/guia1/ejercicios/ejercicio1.php">Ejercicio 1 - Guía 1</a></li>
+                <li><a href="guias/guia2/ejercicios/ejercicio2.php">Ejercicio 2 - Guía 2</a></li>
+                <!-- Agrega más enlaces según sea necesario -->
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>© 2025 Proyecto de Guías NR221019</p>
+    </footer>
+</body>
+</html>
